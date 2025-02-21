b9ae6bb4bd73b108c5228963330be2aa2277e6c9
PandaFalls2004 <129922943+PandaFalls2004@users.noreply.github.com>
2025-02-20 23:52:44 -0800

    step 2

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
  // Set your Mapbox access token here
- mapboxgl.accessToken = 'pk.eyJ1IjoicGFuZGEyMDA0IiwiYSI6ImNtN2VlNnNhNDA3bm8yenE0M2tjZjVhN3cifQ.r8h8lqLIltZ4r227nmaX1A';
+ mapboxgl.accessToken = 'pk.eyJ1IjoicGFuZGEyMDA0IiwiYSI6ImNtN2VnODUyYzBkazYybG9xNjBuZjc0ZTQifQ.dDhc4DynAY1yTeoVvo3Cfw';
 
  // Initialize the map
  const map = new mapboxgl.Map({
@@ -10,3 +10,38 @@
    minZoom: 5, // Minimum allowed zoom
    maxZoom: 18 // Maximum allowed zoom
  });
+ map.on('load', () => { 
+  //code 
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+  });
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4
+    }
+  });
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson?...'
+  });
+  map.addLayer({
+    id: 'lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4
+    }
+  });
+  
+});
+
+
+
